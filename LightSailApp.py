c9071bbd860876dd11892c5a1b322b91c220b362
hashem78 <hashem.olayano@gmail.com>
2020-09-16 23:45:23 +0300

    Get some files renamed

--- LightSailApp.py
+++ LightSailApp.py
@@ -1,8 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+
+import base64
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/home/ubuntu/flaskapp/static'
+
+
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips, concatenate_audioclips
 import parselmouth
 from parselmouth.praat import call, run_file
-
-import os
 import subprocess
 
 
@@ -107,3 +115,41 @@ def video_cut_at(filename, cuts):
                 cuts[i][1], cuts[i + 1][0])
             final_video = concatenate_videoclips([final_video, a])
     return final_video
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload2_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        return 'file uploaded successfully'
+
+
+@app.route('/uploadWAV', methods=['GET', 'POST'])
+def uploadWAV():
+    content = request.get_json(silent=True)
+
+    wav_file = open("to_be_proc.wav", "wb+")
+    decode_string = base64.b64decode(content['file'])
+    wav_file.write(decode_string)
+    wav_file.close()
+    
+    silences = extract_silences_audio_ml('to_be_proc')
+    audio_cut_at('to_be_proc.wav',silences)
+    
+    encodedWAV = base64.b64encode(open("done_proc.wav",'rb').read())
+
+    ### Encode the file
+    return jsonify(
+        file = encodedWAV,
+        response = "HTTP 200"
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
